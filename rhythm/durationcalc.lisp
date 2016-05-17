000490a0dd3b2bee615d8ee3dc97821216d39d37
Jeremy Mates <jmates@cpan.org>
2016-05-17 02:12:42 +0000

    Ability to test (buggy) duration algo.

--- rhythm/durationcalc.lisp
+++ rhythm/durationcalc.lisp
@@ -3,6 +3,8 @@
 ;;; given a number of notes, and a base duration, calculates what
 ;;; duration of note and if necessary tuplet is required to fit that
 ;;; given number of notes exactly into the given base duration.
+;;; NOTE buggy though usable for even divisions, 2/3, 4/5, and a few
+;;; other fractional values...
 (defun breakdown (n dur)
   (let ((pow) (rem))
     (setf (values pow rem) (truncate (/ (log n) (log 2))))
@@ -10,10 +12,30 @@
       (* dur (expt 2 pow))              ; duration required for note(s)
       (if (plusp rem) (/ (1- n) n)))))  ; tuplet, if any
 
-(loop for i from 1 to 20 do 
-      (format t "~d ~{~d ~a~}~%" i (breakdown i +baseduration+)))
+;;; and the rest of this is testing the above assertion for correctness
+(defun audit-lilypond (i file)
+  (let ((results (run-program "./ly-audit-beam"
+                              :arguments (list file)
+                              :input nil :output :stream)))
+    (let ((out (read-line results nil "")))
+      (if (plusp (length out))
+        (format t "NOT OK ~d ~a~%" i out)))))
 
-; 17 64 16/17 indicates that to fit 17 notes into a quarter note, 64th
-; notes must be the duration, and a tuplet of 16/17. Confirm with
-; lilypond via:
-;   ly-fu --open '\times 16/17 { c64 c c c c c c c c c c c c c c c c } r4 r4 r4 | r r r r'
+(defun out-filename (i)
+  (format nil "durcalc-temp~a.ly" i))
+
+(defun write-lilypond (stream i dur tuplet)
+  (format stream "\\version \"2.18.2\"~%\\relative c { ")
+  (dotimes (r +baseduration+)           ; fill up a measure
+    (if tuplet (format stream "\\times ~d { " tuplet))
+    (dotimes (x i) (format stream "c~d " dur))
+    (if tuplet (format stream "} ")))
+  (format stream "| }")                 ; for the measure bar check
+  (fresh-line stream))
+
+(loop for i from 1 to 32 do 
+      (destructuring-bind (dur tuplet) (breakdown i +baseduration+)
+        (let ((fname (out-filename i)))
+          (with-open-file (stream fname :direction :output)
+            (write-lilypond stream i dur tuplet))
+          (audit-lilypond i fname))))
